642ec20c33cfdfe31ebc6466960eed78322a484b
Selene <selenedanyluk@gmail.com>
2026-03-31 09:58:49 -0300

    ADD new book component

--- book_champion/src/components/bookItem/BookItem.jsx
+++ book_champion/src/components/bookItem/BookItem.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-const BookItem = ({ title }) => {
-
-
+const BookItem = ({ title, author, rating, pageCount, imageUrl, available }) => {
     return (
         <>
-            <div>BookItem</div>
-            <p> Título del libro: {title}</p>
+            <Card className="mx-3" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={imageUrl} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Subtitle>{author}</Card.Subtitle>
+                    <div>
+                        <div>{rating} estrellas</div>
+                        <p>{pageCount} páginas</p>
+                        <p>{available ? "Disponible" : "Reservado"}</p>
+                    </div>
+                    <Button variant="dark">Cambiar titulo</Button>
+                </Card.Body>
+            </Card>
         </>
     )
 }
---
+++ book_champion/src/components/books/Books.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import BookItem from '../bookItem/BookItem'
+
+const Books = ({ books }) => {
+    return (
+        <div className="d-flex justify-content-center flex-wrap">
+            {books.map((book) =>
+                <BookItem
+                    title={book.title}
+                    author={book.author}
+                    rating={book.rating}
+                    pageCount={book.pageCount}
+                    imageUrl={book.imageUrl}
+                    available={book.available}
+                />
+            )}
+        </div>
+    )
+}
+
+
+
+export default Books
